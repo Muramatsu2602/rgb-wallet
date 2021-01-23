580103f1f4c63ede1fe84938fa1ab76943d6a50b
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-23 15:34:57 -0300

    installing axios and components seem to be working

--- web/src/routes.js
+++ web/src/routes.js
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import CreateUser from "./pages/CreateUser";
-import Admin from "./pages/Admin";
-import User from "./pages/Login";
+// import CreateUser from "./pages/CreateUser";
+// import Admin from "./pages/Admin";
+// import User from "./pages/Login";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/admin" component={Admin} />
+        {/* <Route path="/admin" component={Admin} />
         <Route path="/user" component={User} />
-        <Route path="/admin/create" component={CreateUser} />
+        <Route path="/admin/create" component={CreateUser} /> */}
       </Switch>
     </BrowserRouter>
   );
