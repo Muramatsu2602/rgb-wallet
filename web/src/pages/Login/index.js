57c050f0d04fbfde6a4bf498b95d3cb35c538799
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-24 15:14:28 -0300

    trying to use accordeon with password field

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -7,7 +7,8 @@ import logoImg from "../../assets/images/login-logo.svg";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Button, Container, Form, Row, Col } from "react-bootstrap/";
+import { Button, Container, Form, Row, Col, Accordion } from "react-bootstrap/";
+import Switch from "react-switch";
 
 export default function Login() {
   return (
@@ -21,7 +22,7 @@ export default function Login() {
         <div className="form-portion">
           <h1>Login</h1>
 
-          <Form>
+          <Form onSubmit={() => {}}>
             <Form.Group controlId="formBasicEmail">
               <Form.Label className="custom-lbl">Nome</Form.Label>
               <Form.Control
@@ -32,14 +33,22 @@ export default function Login() {
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label className="custom-lbl">Senha</Form.Label>
-              <Form.Control
-                className="custom-input"
-                type="password"
-                placeholder="Digite sua senha"
-              />
-            </Form.Group>
+            <Accordion defaultActiveKey="0">
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                
+              </Accordion.Toggle>
+
+              <Accordion.Collapse eventKey="1">
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label className="custom-lbl">Senha</Form.Label>
+                  <Form.Control
+                    className="custom-input"
+                    type="password"
+                    placeholder="Digite sua senha"
+                  />
+                </Form.Group>
+              </Accordion.Collapse>
+            </Accordion>
 
             <div className="form-buttons">
               <Button className="custom-btn" variant="primary" type="submit">
