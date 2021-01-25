0e24916a62ef3442254e0ca1343fe0cc247f8236
Beneti <vitor.beneti@gmail.com>
2021-01-25 14:50:38 -0300

    Authentication protoype

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import {isAuthenticated} from './auth'
 import Login from "./pages/Login";
 import User from "./pages/User";
 // import CreateUser from "./pages/CreateUser";
 // import Admin from "./pages/Admin";
 
+const PrivateRoute = ({component: Component, ...rest}) =>(
+  <Route 
+  {...rest} 
+  render = {props => 
+    isAuthenticated() ? (
+      <Component { ... props} />
+    ) : (
+      <Redirect to = {{pathname: '/', state : {from: props.location}}}/> 
+    )
+    
+  }/>
+
+)
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/user" component={User} />
+        <Route exact path="/" exact component={Login} />
+        //private route to user
+        <PrivateRoute exact path="/user" component={User} /> 
 
         {/* <Route path="/admin" component={Admin} />
         <Route path="/admin/create" component={CreateUser} /> */}
