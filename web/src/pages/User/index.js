a0f8c2a873c35435811fdfbff5b0de012b6891ce
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-24 21:07:49 -0300

    header is now a component and trying to use Onclick stop progation on a Link component

--- web/src/pages/User/index.js
+++ web/src/pages/User/index.js
@@ -1,34 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { FaAngleDown } from "react-icons/fa";
-import CornerLogo from "../../assets/images/corner-logo.svg";
-import CornerArrow from "../../assets/images/corner-arrow-left.svg";
 
-import Swal from "sweetalert2";
-
+import Header from "../../Components/Header/index";
 import CustomToggle from "../../Components/CustomToggle/index";
+
 import { Button, Table, Accordion } from "react-bootstrap/";
 
 export default function User() {
   return (
     <div id="user-page">
       <div className="content-wrapper">
-        <div className="header-wrapper">
-          <div className="top-icons">
-            <Link to="/" className="exit-app">
-              <img src={CornerArrow} id="arrow" alt="arrow de retorno" />
-            </Link>
-            <img src={CornerLogo} alt="logo RGBWallet" />
-          </div>
-          <div className="hello-user">
-            <h1>Olá, usuário</h1>
-            <p>Esta é sua carteira virtual</p>
-          </div>
-        </div>
+        <Header />
         <div className="body-wrapper">
           <div className="wallet-wrapper">
             <div className="current-balance">
