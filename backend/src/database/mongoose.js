9415adb82e6157d00885f6767e71791bf67eb54f
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-28 19:51:31 -0300

    no Scrollbars + mongoose.js according to Rabiola

--- backend/src/database/mongoose.js
+++ backend/src/database/mongoose.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
+const uri = "CODIGO DO MONGO"
 // const uri = "mongodb+srv://vitorbeneti:powerguido1234594@rgbwallet.dgsze.mongodb.net/rgbwallet?retryWrites=true&w=majority";
-const uri = "mongodb/kenzoooo";
 
 
 mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-
+  useNewUrlParser: true,
+  useCreateIndex: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
