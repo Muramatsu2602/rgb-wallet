f60b6ef979679cd6a3840521155cd9c6d695b9bd
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-30 22:46:07 -0300

    front-end officially complete!

--- web/src/components/ToggleButton/index.js
+++ web/src/components/ToggleButton/index.js
@@ -21,6 +21,9 @@ class ToggleButton extends Component {
           checked={this.state.checked}
           onColor="#04d361"
           offColor="#e33d3d"
+          offHandleColor='#e6e6f0'
+          onHandleColor='#e6e6f0'
+
         />
       </div>
     );
